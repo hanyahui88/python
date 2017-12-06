0b7afed5c58e01aabca8f8b7cf93478756d5cc23
alvin <hanyahui88@163.com>
2017-12-06 22:39:52 +0800

    continue learn

--- study3/collection1.py
+++ study3/collection1.py
@@ -1,3 +1,16 @@
+def wirteFile():
+    file = open("data.json", "a")
+    input("start")
+    while True:
+        word = input("enter word(',' to quit)")
+        if word != '.':
+            file.write(word)
+        else:
+            break
+    file.close()
+
+
+wirteFile()
 print([x ** 2 for x in range(6)])
 
 print(max(len(x.strip()) for x in open("data.json")))
